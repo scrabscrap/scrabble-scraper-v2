e6b773566fc8a6f3f2c27fca9e8ea0402cbc7195
scrabscrap <scrabblescraper@gmail.com>
2022-07-06 13:09:58 +0200

    test: use logging in check_led

--- python/test/check_led.py
+++ python/test/check_led.py
@@ -1,32 +1,37 @@
+import logging
 import time
 import unittest
-from led import LEDEnum, LED
+
+from led import LED, LEDEnum
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(asctime)s [%(levelname)-5.5s] %(funcName)-20s: %(message)s')
 
 
 class LedTestCase(unittest.TestCase):
 
     def test_led(self):
         LED.switch_on({LEDEnum.red, LEDEnum.yellow, LEDEnum.green})
-        print('led on')
+        logging.debug('led on')
         time.sleep(2)
 
         LED.switch_on({})  # type: ignore
-        print('led off')
+        logging.debug('led off')
         time.sleep(2)
 
         LED.blink_on({LEDEnum.red, LEDEnum.yellow, LEDEnum.green})
-        print('led blink')
+        logging.debug('led blink')
         time.sleep(4)
 
         LED.switch_on({})  # type: ignore
-        print('led off')
+        logging.debug('led off')
         time.sleep(2)
 
         LED.switch_on({LEDEnum.red, LEDEnum.yellow, LEDEnum.green})
-        print('led on')
+        logging.debug('led on')
         time.sleep(2)
 
-        print('end')
+        logging.debug('end')
 
 
 if __name__ == '__main__':
