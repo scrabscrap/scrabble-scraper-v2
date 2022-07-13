57cd31100775725c2a74714af4d253d53b9479ac
scrabscrap <scrabblescraper@gmail.com>
2022-07-13 21:28:02 +0200

    style: code format

--- python/src/board/tiles.py
+++ python/src/board/tiles.py
@@ -1,17 +1,17 @@
 """
  This file is part of the scrabble-scraper distribution (https://github.com/scrabscrap/scrabble-scraper)
  Copyright (c) 2022 Rainer Rohloff.
- 
- This program is free software: you can redistribute it and/or modify  
- it under the terms of the GNU General Public License as published by  
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
  the Free Software Foundation, version 3.
 
- This program is distributed in the hope that it will be useful, but 
- WITHOUT ANY WARRANTY; without even the implied warranty of 
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  General Public License for more details.
 
- You should have received a copy of the GNU General Public License 
+ You should have received a copy of the GNU General Public License
  along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
@@ -38,7 +38,7 @@ bag_as_list = sum([[k] * bag[k] for k in bag], [])
 tiles: List[OneTile] = []
 
 
-def load_tiles(filepath: Optional[str]=None) -> List[OneTile]:
+def load_tiles(filepath: Optional[str] = None) -> List[OneTile]:
     tiles.clear()
     if filepath is None:
         filepath = (os.path.dirname(__file__) or '.') + '/img'
