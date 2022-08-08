9ab2a0241cdc97f9bc0c931e1c2ee10c15684013
scrabscrap <scrabblescraper@gmail.com>
2022-08-08 14:45:32 +0200

    feat: add warp config to classic board

--- python/src/classic.py
+++ python/src/classic.py
@@ -22,6 +22,7 @@ import imutils
 import numpy as np
 from vlogging import VisualRecord
 
+from config import config
 from game_board.board import GRID_H, GRID_W, get_x_position, get_y_position
 
 Mat = np.ndarray[int, np.dtype[np.generic]]
@@ -34,43 +35,48 @@ class Classic:
 
     @staticmethod
     def warp(__image):
-        # based on: https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
-        (blue, _, _) = cv2.split(__image.copy())
 
-        # Otsu's thresholding after Gaussian filtering
-        blur = cv2.GaussianBlur(blue, (5, 5), 0)
-        ret3, th3 = cv2.threshold(
-            blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        if config.WARP_COORDINATES is not None:
+            rect = np.array(config.WARP_COORDINATES, dtype="float32")
+        else:
+            # based on: https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+            (blue, _, _) = cv2.split(__image.copy())
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
-        dilated = cv2.dilate(th3, kernel)
+            # Otsu's thresholding after Gaussian filtering
+            blur = cv2.GaussianBlur(blue, (5, 5), 0)
+            ret3, th3 = cv2.threshold(
+                blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-        cnts = cv2.findContours(
-            dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:1]
-        c = cnts[0]
-        peri = 1
-        approx = cv2.approxPolyDP(c, peri, True)
-        while len(approx) > 4:
-            peri += 1
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+            dilated = cv2.dilate(th3, kernel)
+
+            cnts = cv2.findContours(
+                dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:1]
+            c = cnts[0]
+            peri = 1
             approx = cv2.approxPolyDP(c, peri, True)
+            while len(approx) > 4:
+                peri += 1
+                approx = cv2.approxPolyDP(c, peri, True)
 
-        pts = approx.reshape(4, 2)
-        rect = np.zeros((4, 2), dtype="float32")
+            pts = approx.reshape(4, 2)
+            rect = np.zeros((4, 2), dtype="float32")
 
-        # the top-left point has the smallest sum whereas the
-        # bottom-right has the largest sum
-        s = pts.sum(axis=1)
-        rect[0] = pts[np.argmin(s)]
-        rect[2] = pts[np.argmax(s)]
+            # the top-left point has the smallest sum whereas the
+            # bottom-right has the largest sum
+            s = pts.sum(axis=1)
+            rect[0] = pts[np.argmin(s)]
+            rect[2] = pts[np.argmax(s)]
 
-        # compute the difference between the points -- the top-right
-        # will have the minumum difference and the bottom-left will
-        # have the maximum difference
-        diff = np.diff(pts, axis=1)
-        rect[1] = pts[np.argmin(diff)]
-        rect[3] = pts[np.argmax(diff)]
+            # compute the difference between the points -- the top-right
+            # will have the minumum difference and the bottom-left will
+            # have the maximum difference
+            diff = np.diff(pts, axis=1)
+            rect[1] = pts[np.argmin(diff)]
+            rect[3] = pts[np.argmax(diff)]
+
         # now that we have our rectangle of points, let's compute
         # the width of our new image
         (tl, tr, br, bl) = rect
