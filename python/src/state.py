d00e6e617c431febb2419d72e534526120f0be87
scrabscrap <scrabblescraper@gmail.com>
2022-09-23 16:20:14 +0200

    refactor: use constants in state

--- python/src/state.py
+++ python/src/state.py
@@ -29,15 +29,32 @@ from threadpool import pool
 from util import Singleton
 
 
+# states
+START = 'START'
+S0 = 'S0'
+S1 = 'S1'
+P0 = 'P0'
+P1 = 'P1'
+# buttons
+GREEN = 'GREEN'
+YELLOW = 'YELLOW'
+RED = 'RED'
+DOUBT0 = 'DOUBT0'
+DOUBT1 = 'DOUBT1'
+RESET = 'RESET'
+REBOOT = 'REBOOT'
+CONFIG = 'CONFIG'
+
+
 class State(metaclass=Singleton):
 
     def __init__(self, cam=None, watch: Optional[ScrabbleWatch] = None) -> None:
-        self.current_state: str = 'START'
+        self.current_state: str = START
         self.watch: ScrabbleWatch = watch if watch is not None else ScrabbleWatch()
         self.cam = cam
         self.last_submit = None  # last submit to thread pool; waiting for processing of the last move
-        self.bounce = {'GREEN': .0, 'RED': .0, 'YELLOW': .0, 'DOUBT0': .0,
-                       'DOUBT1': .0, 'RESET': .0, 'CONFIG': .0, 'REBOOT': .0}
+        self.bounce = {GREEN: .0, RED: .0, YELLOW: .0, DOUBT0: .0,
+                       DOUBT1: .0, RESET: .0, CONFIG: .0, REBOOT: .0}
         self.game: Game = Game(None)
         atexit.register(self._atexit)
 
@@ -49,26 +66,26 @@ class State(metaclass=Singleton):
         """Game can be started"""
         start_of_game()
         self.watch.display.show_ready()
-        self.current_state = 'START'
+        self.current_state = START
         return self.current_state
 
     def do_start0(self) -> str:
         """Start playing with player 0"""
-        logging.debug(f'{self.current_state} - (start) -> S0')
+        logging.debug(f'{self.current_state} - (start) -> {S0}')
         self.watch.start(0)
         LED.switch_on({LEDEnum.green})  # turn on LED green
-        return 'S0'
+        return S0
 
     def do_start1(self) -> str:
         """Start playing with player 1"""
-        logging.debug(f'{self.current_state} - (start) -> S1')
+        logging.debug(f'{self.current_state} - (start) -> {S1}')
         self.watch.start(1)
         LED.switch_on({LEDEnum.red})  # turn on LED red
-        return 'S1'
+        return S1
 
     def do_move0(self) -> str:
         """analyze players 0 move"""
-        logging.debug(f'{self.current_state} - (move) -> S1')
+        logging.debug(f'{self.current_state} - (move) -> {S1}')
         _, t0, _, t1, _ = self.watch.get_status()
         # next player
         self.watch.start(1)
@@ -76,11 +93,11 @@ class State(metaclass=Singleton):
 
         picture = self.cam.read()  # type: ignore
         self.last_submit = pool.submit(move, self.last_submit, self.game, picture, 0, (t0, t1))
-        return 'S1'
+        return S1
 
     def do_move1(self) -> str:
         """analyze players 1 move"""
-        logging.debug(f'{self.current_state} - (move) -> S0')
+        logging.debug(f'{self.current_state} - (move) -> {S0}')
         _, t0, _, t1, _ = self.watch.get_status()
         # next player
         self.watch.start(0)
@@ -88,88 +105,86 @@ class State(metaclass=Singleton):
 
         picture = self.cam.read()  # type: ignore
         self.last_submit = pool.submit(move, self.last_submit, self.game, picture, 1, (t0, t1))
-        return 'S0'
+        return S0
 
     def do_pause0(self) -> str:
         """pause pressed while player 0 is active"""
-        logging.debug(f'{self.current_state} - (pause) -> P0')
+        logging.debug(f'{self.current_state} - (pause) -> {P0}')
         self.watch.pause()
         LED.switch_on({LEDEnum.green, LEDEnum.yellow})  # turn on LED green, yellow
-        return 'P0'
+        return P0
 
     def do_pause1(self) -> str:
         """pause pressed while player 1 is active"""
-        logging.debug(f'{self.current_state} - (pause) -> P1')
+        logging.debug(f'{self.current_state} - (pause) -> {P1}')
         self.watch.pause()
         LED.switch_on({LEDEnum.red, LEDEnum.yellow})  # turn on LED red, yellow
-        return 'P1'
+        return P1
 
     def do_resume0(self) -> str:
         """resume from pause while player 0 is active"""
-        logging.debug(f'{self.current_state} - (resume) -> S0')
+        logging.debug(f'{self.current_state} - (resume) -> {S0}')
         self.watch.resume()
         LED.switch_on({LEDEnum.green})  # turn on LED green
-        return 'S0'
+        return S0
 
     def do_resume1(self) -> str:
         """resume from pause while player 1 is active"""
-        logging.debug(f'{self.current_state} - (resume) -> S1')
+        logging.debug(f'{self.current_state} - (resume) -> {S1}')
         self.watch.resume()
         LED.switch_on({LEDEnum.red})  # turn on LED red
-        return 'S1'
+        return S1
 
     def do_valid_challenge0(self) -> str:
         """player 0 has a valid challenge for the last move from player 1"""
-        logging.debug(f'{self.current_state} - (valid challenge) -> P0')
+        logging.debug(f'{self.current_state} - (valid challenge) -> {P0}')
         _, t0, _, t1, _ = self.watch.get_status()
         self.watch.display.add_remove_tiles(1)  # player 1 has to remove the last move
         self.last_submit = pool.submit(valid_challenge, self.last_submit, self.game, 0, (t0, t1))
         LED.switch_on({LEDEnum.green, LEDEnum.yellow})  # turn on LED green, yellow
-        return 'P0'
+        return P0
 
     def do_valid_challenge1(self) -> str:
         """player 1 has a valid challenge for the last move from player 0"""
-        logging.debug(f'{self.current_state} - (valid challenge) -> P1')
+        logging.debug(f'{self.current_state} - (valid challenge) -> {P1}')
         _, t0, _, t1, _ = self.watch.get_status()
         self.watch.display.add_remove_tiles(0)  # player 0 has to remove the last move
         self.last_submit = pool.submit(valid_challenge, self.last_submit, self.game, 1, (t0, t1))
         LED.switch_on({LEDEnum.red, LEDEnum.yellow})  # turn on LED red, yellow
-        return 'P1'
+        return P1
 
     def do_invalid_challenge0(self) -> str:
         """player 0 has an invalid challenge for the last move from player 1"""
         logging.debug(
-            f'{self.current_state} - (invalid challenge) -> P0 (-{config.MALUS_DOUBT:2d})')  # -10
+            f'{self.current_state} - (invalid challenge) -> {P0} (-{config.MALUS_DOUBT:2d})')  # -10
         _, t0, _, t1, _ = self.watch.get_status()
         self.watch.display.add_malus(0)  # player 0 gets a malus
         self.last_submit = pool.submit(invalid_challenge, self.last_submit, self.game, 0, (t0, t1))
         LED.switch_on({LEDEnum.green, LEDEnum.yellow})  # turn on LED green
-        return 'P0'
+        return P0
 
     def do_invalid_challenge1(self) -> str:
         """player 0 has an invalid challenge for the last move from player 1"""
         logging.debug(
-            f'{self.current_state} - (invalid challenge) -> P1 (-{config.MALUS_DOUBT:2d})')  # -10
+            f'{self.current_state} - (invalid challenge) -> {P1} (-{config.MALUS_DOUBT:2d})')  # -10
         _, t0, _, t1, _ = self.watch.get_status()
         self.watch.display.add_malus(1)  # player 1 gets a malus
         self.last_submit = pool.submit(invalid_challenge, self.last_submit, self.game, 1, (t0, t1))
         LED.switch_on({LEDEnum.red, LEDEnum.yellow})  # turn on LED red, yellow
-        return 'P1'
+        return P1
 
     def do_reset(self) -> str:
         """Resets state and game to default"""
-        logging.debug(f'{self.current_state} - (reset) -> START')
+        logging.debug(f'{self.current_state} - (reset) -> {START}')
         LED.switch_on({})  # type: ignore
         self.watch.reset()
         end_of_game(None, self.game)
         self.game.new_game()
-        current_state = 'START'
-        self.do_ready()
-        return current_state
+        return self.do_ready()
 
     def do_reboot(self) -> str:
         """Perform a reboot"""
-        logging.debug(f'{self.current_state} - (reboot) -> START')
+        logging.debug(f'{self.current_state} - (reboot) -> {START}')
         self.watch.display.show_boot()  # Display message REBOOT
         LED.switch_on({})  # type: ignore
         end_of_game(self.last_submit, self.game)
@@ -180,7 +195,7 @@ class State(metaclass=Singleton):
 
     def do_config(self) -> str:
         """??? necessary ???"""
-        logging.debug(f'{self.current_state} - (config) -> START')
+        logging.debug(f'{self.current_state} - (config) ignore')
         # self.watch.reset()
         # self.watch.display.show_config()  # Display message CONFIG
         # todo: check for upload
@@ -198,7 +213,7 @@ class State(metaclass=Singleton):
             if press < self.bounce[button] + 0.1:
                 pass
             else:
-                self.current_state = self.state[(self.current_state, button.lower())](self)
+                self.current_state = self.state[(self.current_state, button)](self)
         except KeyError:
             logging.debug('Key Error - ignore')
 
@@ -211,27 +226,27 @@ class State(metaclass=Singleton):
 
     # START, pause => not supported
     state: dict[tuple[str, str], Callable] = {
-        ('START', 'green'): do_start1,
-        ('START', 'red'): do_start0,
-        ('START', 'reset'): do_reset,
-        ('START', 'reboot'): do_reboot,
-        ('START', 'config'): do_config,
-        ('S0', 'green'): do_move0,
-        ('S0', 'yellow'): do_pause0,
-        ('P0', 'red'): do_resume0,
-        ('P0', 'yellow'): do_resume0,
-        ('P0', 'doubt0'): do_valid_challenge0,
-        ('P0', 'doubt1'): do_invalid_challenge0,
-        ('P0', 'reset'): do_reset,
-        ('P0', 'reboot'): do_reboot,
-        ('P0', 'config'): do_config,
-        ('S1', 'red'): do_move1,
-        ('S1', 'yellow'): do_pause1,
-        ('P1', 'green'): do_resume1,
-        ('P1', 'yellow'): do_resume1,
-        ('P1', 'doubt1'): do_valid_challenge1,
-        ('P1', 'doubt0'): do_invalid_challenge1,
-        ('P1', 'reset'): do_reset,
-        ('P1', 'reboot'): do_reboot,
-        ('P1', 'config'): do_config,
+        (START, GREEN): do_start1,
+        (START, RED): do_start0,
+        (START, RESET): do_reset,
+        (START, REBOOT): do_reboot,
+        (START, CONFIG): do_config,
+        (S0, GREEN): do_move0,
+        (S0, YELLOW): do_pause0,
+        (P0, RED): do_resume0,
+        (P0, YELLOW): do_resume0,
+        (P0, DOUBT0): do_valid_challenge0,
+        (P0, DOUBT1): do_invalid_challenge0,
+        (P0, RESET): do_reset,
+        (P0, REBOOT): do_reboot,
+        (P0, CONFIG): do_config,
+        (S1, RED): do_move1,
+        (S1, YELLOW): do_pause1,
+        (P1, GREEN): do_resume1,
+        (P1, YELLOW): do_resume1,
+        (P1, DOUBT1): do_valid_challenge1,
+        (P1, DOUBT0): do_invalid_challenge1,
+        (P1, RESET): do_reset,
+        (P1, REBOOT): do_reboot,
+        (P1, CONFIG): do_config,
     }
