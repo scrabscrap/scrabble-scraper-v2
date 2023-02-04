d06efa3365068be5713cc07d13e10f0bc84819ba
scrabscrap <scrabblescraper@gmail.com>
2023-02-04 16:50:00 +0100

    fix(react): names with blanks on move display

--- react/src/display/Moves.js
+++ react/src/display/Moves.js
@@ -9,37 +9,39 @@ class Moves extends React.Component {
     }
     if (this.props.moves != null) {
       for (const [key, value] of Array.from(this.props.moves.entries())) {
-        let sp = value.split(' ')
+        let name = value.slice(1, value.indexOf(':') + 1)
+        let rest = value.slice(value.indexOf(':') + 1).trim()
+        let sp = rest.split(' ')
         let img_link = "/web/image-" + (key + 1) + ".jpg"
-        if (sp.length === 4) {
+        if (sp.length === 3) {
           items.push(<tr key={key + 1}>
             <td><a href={img_link} target="_scrabscrap_board">{key + 1}</a></td>
+            <td>{name}</td>
+            <td>{sp[0]}</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td style={align}>{sp[1]}</td>
+            <td style={align}>{sp[2]}</td>
+          </tr>)
+        } else if (sp.length === 4) {
+          items.push(<tr key={key + 1}>
+            <td><a href={img_link} target="_scrabscrap_board">{key + 1}</a></td>
+            <td>{name}</td>
             <td>{sp[0]}</td>
             <td>{sp[1]}</td>
-            <td>&nbsp;</td>
             <td>&nbsp;</td>
             <td style={align}>{sp[2]}</td>
             <td style={align}>{sp[3]}</td>
           </tr>)
-        } else if (sp.length === 5) {
-          items.push(<tr key={key + 1}>
-            <td><a href={img_link} target="_scrabscrap_board">{key + 1}</a></td>
-            <td>{sp[0]}</td>
-            <td>{sp[1]}</td>
-            <td>{sp[2]}</td>
-            <td>&nbsp;</td>
-            <td style={align}>{sp[3]}</td>
-            <td style={align}>{sp[4]}</td>
-          </tr>)
         } else {
           items.push(<tr key={key + 1}>
             <td><a href={img_link} target="_scrabscrap_board">{key + 1}</a></td>
+            <td>{name}</td>
             <td>{sp[0]}</td>
             <td>{sp[1]}</td>
             <td>{sp[2]}</td>
-            <td>{sp[3]}</td>
+            <td style={align}>{sp[3]}</td>
             <td style={align}>{sp[4]}</td>
-            <td style={align}>{sp[5]}</td>
           </tr>)
         }
         // items.push(<li key={key}>{value}</li>)
