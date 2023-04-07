29494d063a159f45fa5d53f98a6762bae9ce4042
scrabscrap <scrabblescraper@gmail.com>
2023-04-07 09:46:21 +0200

    chore: small and simple qa.sh script

---
+++ python/qa.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+  source .venv/bin/activate
+
+while getopts c:h flag
+do
+    case "${flag}" in
+        h)
+          echo "usage qa.sh"
+          echo "always run flake8, mypy, pylint"
+          echo "-h help"
+          echo "-c run coverage"
+          exit 0
+          ;;
+        c) 
+          echo "*** run coverage"
+          coverage run -m unittest discover  &> /dev/null
+          coverage report
+          ;;
+    esac
+done
+
+  echo "*** run flake8"
+  flake8 src/*.py src/hardware/*.py src/game_board/*.py simulator/*.py --max-line-length=128 --ignore=E402 --exclude="ignore/*" --count --show-source --statistics
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    echo "### flake8 returns $retVal"
+  fi
+
+  echo "*** run mypy"
+  mypy src/*.py src/hardware/*.py src/game_board/*.py simulator/*.py
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    echo "### mypy returns $retVal"
+  fi
+  
+  echo "*** run pylint"
+  pylint src/*.py src/hardware/*.py src/game_board/*.py simulator/*.py
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    echo "### pylint returns $retVal"
+  fi
+
