cc89bda6878e6a45c45f211fba336e1ac8878967
scrabscrap <scrabblescraper@gmail.com>
2023-11-04 22:08:43 +0100

    chore: nodejs new installation method

--- scripts/upgrade-system.sh
+++ scripts/upgrade-system.sh
@@ -70,14 +70,21 @@ echo "####################################################################"
 echo "## Upgrade nodejs npm                                             ##"
 echo "####################################################################"
 
-if [[ $(/usr/bin/node -v) == *"v19"* ]]; then
-    echo "node 19 is installed, skipping..."
+if [[ $(/usr/bin/node -v) == *"v21"* ]]; then
+    echo "node 21 is installed, skipping..."
 else
-    sudo apt-get remove -yq nodejs
+    sudo apt-get -yq purge nodejs
+    sudo rm -r /etc/apt/sources.list.d/nodesource.list
+    sudo rm -r /etc/apt/keyrings/nodesource.gpg    
     sudo rm -rf /usr/local/bin/node*
     sudo rm -rf /usr/local/bin/npm*
-    sudo rm -rf /etc/apt/sources.list.d/nodesource.list
-    curl -sL https://deb.nodesource.com/setup_19.x | sudo bash -
+
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    NODE_MAJOR=21
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update
     sudo apt-get install -yq nodejs
 fi
 
