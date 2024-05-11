f6ebd089e25f3fef941eeb795b55f0eac22d155b
scrabscrap <scrabblescraper@gmail.com>
2024-05-12 00:30:27 +0200

    feat: highlight bag display if there are no tiles in the tiles bag

--- react/src/display/Bag.jsx
+++ react/src/display/Bag.jsx
@@ -6,14 +6,18 @@ class Bag extends Component {
     render() {
         const items = [];
 
+        let panelclass = 'card-body bag-body'
         if (this.props.bag != null) {
             for (const [key, value] of this.props.bag.entries()) {
                 items.push(<span key={key} className='tile'>{value}</span>)
             }
+            if (this.props.bag.length <= 14) {
+                panelclass = panelclass + ' bg-warning'
+            }
         }
         return (
             <div className='card my-1'>
-                <div className='card-body bag-body'>{items}</div>
+                <div className={panelclass}>{items}</div>
             </div>
         );
     }
