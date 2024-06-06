bf11d6e27b3ab17995fde9d19b19423300897eba
scrabscrap <scrabblescraper@gmail.com>
2024-06-06 21:53:17 +0200

    fix(react): set background based on the theme

--- react/src/display/Display.jsx
+++ react/src/display/Display.jsx
@@ -19,7 +19,13 @@ class Display extends Component {
         if (data.op === 'S1') { player_on_move = (data.settings.websocket) ? '1' : '0' }
 
         let root_css = 'container-fluid '
-        { root_css += (data.settings.theme2020) ? 'theme2020' : 'theme2012'}
+        if (data.settings.theme2020) {
+            document.body.style.backgroundColor = '#00607d'
+            root_css += 'theme2020'
+        } else {
+            document.body.style.backgroundColor = 'darkgreen'
+            root_css += 'theme2012'
+        }
 
         if (data.settings.obs) {
             return (
@@ -58,55 +64,53 @@ class Display extends Component {
             )
         } else {
             return (
-                <div>
-                    <div className={root_css}>
-                        <div className='row py-1'>
-                            <div className='col-4'>
-                                <Player counter={(player_on_move === '0') && data.settings.websocket}
-                                    unknown_move={data.unknown_move}
-                                    color={player_on_move === '0' ? 'bg-info' : data.op === 'P0' ? 'bg-warning' : ''}
-                                    name={data.name1} score={data.score1} time={data.clock1} on_move={(player_on_move === '0')}
-                                    player_color='text-success' />
-                            </div>
-                            <div className='col'>
-                                <Header time={this.props.state.time} highlight_reload={this.props.state.highlight_reload}
-                                    settings={this.props.state.settings} updateSettings={this.props.updateSettings} />
-                            </div>
-                            <div className='col-4'>
-                                <Player counter={(player_on_move === '1') && data.settings.websocket}
-                                    unknown_move={data.unknown_move}
-                                    color={player_on_move === '1' ? 'bg-info' : data.op === 'P1' ? 'bg-warning' : ''}
-                                    name={data.name2} score={data.score2} time={data.clock2} on_move={(player_on_move === '1')}
-                                    player_color='text-danger' />
+                <div className={root_css}>
+                    <div className='row py-1'>
+                        <div className='col-4'>
+                            <Player counter={(player_on_move === '0') && data.settings.websocket}
+                                unknown_move={data.unknown_move}
+                                color={player_on_move === '0' ? 'bg-info' : data.op === 'P0' ? 'bg-warning' : ''}
+                                name={data.name1} score={data.score1} time={data.clock1} on_move={(player_on_move === '0')}
+                                player_color='text-success' />
+                        </div>
+                        <div className='col'>
+                            <Header time={this.props.state.time} highlight_reload={this.props.state.highlight_reload}
+                                settings={this.props.state.settings} updateSettings={this.props.updateSettings} />
+                        </div>
+                        <div className='col-4'>
+                            <Player counter={(player_on_move === '1') && data.settings.websocket}
+                                unknown_move={data.unknown_move}
+                                color={player_on_move === '1' ? 'bg-info' : data.op === 'P1' ? 'bg-warning' : ''}
+                                name={data.name2} score={data.score2} time={data.clock2} on_move={(player_on_move === '1')}
+                                player_color='text-danger' />
+                        </div>
+                    </div>
+
+                    <div className='row'>
+
+                        <div className='col-sm-6 col-md-4'>
+                            <div className='row py-1'>
+                                <div className='col pr-3'><Picture image={data.image} /></div>
                             </div>
                         </div>
-
-                        <div className='row'>
-
-                            <div className='col-sm-6 col-md-4'>
-                                <div className='row py-1'>
-                                    <div className='col pr-3'><Picture image={data.image} /></div>
-                                </div>
-                            </div>
-                        
-                            <div className='col-auto'>
-                                <div className='row py-1 justify-content-center'>
-                                    <div className='card'>
-                                        <div className='card-body'>
-                                            <Board board={data.board} />
-                                        </div>
+                    
+                        <div className='col-auto'>
+                            <div className='row py-1 justify-content-center'>
+                                <div className='card'>
+                                    <div className='card-body'>
+                                        <Board board={data.board} />
                                     </div>
                                 </div>
                             </div>
-
-                            <div className='col move-panel'>
-                                <Bag bag={data.bag} />
-                                <Moves moves={data.moves} />
-                            </div>
-
                         </div>
+
+                        <div className='col move-panel'>
+                            <Bag bag={data.bag} />
+                            <Moves moves={data.moves} />
+                        </div>
+
                     </div>
-                </div >
+                </div>
             );
         }
     }
