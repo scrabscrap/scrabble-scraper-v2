7a8a7411513db533d6e0ff5e2a46c1fadda27d1f
scrabscrap <scrabblescraper@gmail.com>
2025-06-08 09:29:16 +0200

    fix: invalid image number

--- react/src/display/Moves.jsx
+++ react/src/display/Moves.jsx
@@ -24,7 +24,7 @@ class Moves extends Component {
                 let name = value.slice(1, value.indexOf(':') + 1)
                 let rest = value.slice(value.indexOf(':') + 1).trim()
                 let sp = rest.split(' ')
-                let img_link = './web/image-' + (key + 1) + '.jpg'
+                let img_link = './web/image-' + (key) + '.jpg'
                 if (sp.length === 3) { // move, name, [-|--|(challenge)|(unknown)], [], points, score 
                     items.push(<tr key={key + 1}>
                         <td><a href={img_link} target='_scrabscrap_board'>{key + 1}</a></td>
