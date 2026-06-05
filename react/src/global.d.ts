b9c02e4cc09b722239df9ee83e1f62518fbc2b38
scrabscrap <scrabblescraper@gmail.com>
2026-06-06 01:07:33 +0200

    feat(types): add module declarations for CSS and SVG files

--- react/src/global.d.ts
+++ react/src/global.d.ts
@@ -1,4 +1,10 @@
-interface ImportMeta {
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.less';
+declare module '*.svg';
+
+declare interface ImportMeta {
     env: {
         VITE_APP_VERSION?: string;
         VITE_APP_TAG?: string;
